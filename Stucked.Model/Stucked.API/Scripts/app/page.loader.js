72f9955ca5f74ff33d6b9b9985e6cffb501fb2f4
Carlos Paez <carpaez@gmail.com>
2014-08-22 16:00:59 -0300

    # added loading spinner to map pages

---
+++ Stucked.Model/Stucked.API/Scripts/app/page.loader.js
@@ -0,0 +1,31 @@
+﻿var spinner;
+var target;
+
+var opts = {
+    lines: 13, // The number of lines to draw
+    length: 12, // The length of each line
+    width: 6, // The line thickness
+    radius: 15, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '50%', // Top position relative to parent
+    left: '50%' // Left position relative to parent
+};
+
+function showLoading() {
+    target = document.getElementById('spinner-canvas');
+    spinner = new Spinner(opts).spin(target);
+}
+
+function hideLoading() {
+    spinner.stop();
+    target.style.display = 'none';
+}
